91036cd49d13d6bdf7d861d30a25805cd89109a0
Paul Meffle <meffle.paul@me.com>
2017-09-14 22:49:14 +0200

    Add Collider

---
+++ Collider.hpp
@@ -0,0 +1,68 @@
+#ifndef _COLLIDER_HPP
+#define _COLLIDER_HPP
+
+#include <memory>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include "tinyc2.h"
+#include "Tinyc2Debug.hpp"
+
+class Body {
+public:
+        virtual void moveTo(float x, float y) = 0;
+        virtual float getX() = 0;
+        virtual float getY() = 0;
+};
+
+class CircleBody : public Body {
+public:
+        CircleBody(float r, float x, float y)
+        {
+                body.r = r;
+                body.p.x = x;
+                body.p.y = y;
+        }
+
+        void moveTo(float x, float y)
+        {
+                body.p.x = x;
+                body.p.y = y;
+        }
+
+        float getX()
+        {
+                return body.p.x;
+        }
+
+        float getY()
+        {
+                return body.p.y;
+        }
+
+        c2Circle body;
+};
+
+
+class Collider {
+public:
+        std::shared_ptr<CircleBody> createCircleBody(float r, float x, float y)
+        {
+                auto body = std::make_shared<CircleBody>(r, x, y);
+
+                circleBodies.push_back(body);
+                return body;
+        }
+
+        void draw(sf::RenderTarget& target)
+        {
+                Tinyc2Debug debug(target);
+
+                for (auto& ptr : circleBodies) {
+                        debug.draw(ptr->body);
+                }
+        }
+
+private:
+        std::vector<std::shared_ptr<CircleBody>> circleBodies;
+};
+
+#endif // _COLLIDER_HPP
--- main.cpp
+++ main.cpp
@@ -1,78 +1,10 @@
 #include <SFML/Graphics.hpp>
 #define TINYC2_IMPL
 #include "tinyc2.h"
+#undef TINYC2_IMPL
+#include "Collider.hpp"
 
 #define MC_PER_TICK 15625
-
-class Player : public sf::Drawable {
-        const float speed = 1;
-
-        sf::CircleShape shape;
-
-public:
-        Player()
-        {
-                shape = sf::CircleShape(16);
-                shape.setOrigin(16, 16);
-                body.p.x = 0;
-                body.p.y = 0;
-                body.r = 16;
-        }
-
-        void update()
-        {
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-                        body.p.x -= speed;
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-                        body.p.x += speed;
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-                        body.p.y -= speed;
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-                        body.p.y += speed;
-                }
-        }
-
-        virtual void draw(sf::RenderTarget& target,
-                        sf::RenderStates states) const
-        {
-                sf::Transform translation;
-                translation.translate(body.p.x, body.p.y);
-                states.transform = translation;
-
-                target.draw(shape, states);
-        }
-
-        c2Circle body;
-};
-
-class Obstacle : public sf::Drawable {
-        sf::RectangleShape shape;
-
-public:
-        Obstacle(int width, int height, float x, float y)
-        {
-                shape = sf::RectangleShape(sf::Vector2f(width, height));
-                body.min.x = x;
-                body.min.y = y;
-                body.max.x = x + width;
-                body.max.y = y + height;
-        }
-
-        virtual void draw(sf::RenderTarget& target,
-                        sf::RenderStates states) const
-        {
-                sf::Transform translation;
-                translation.translate(body.min.x, body.min.y);
-                states.transform = translation;
-
-                target.draw(shape, states);
-        }
-
-        c2AABB body;
-};
 
 int main(int argc, char* argv[])
 {
@@ -81,8 +13,9 @@ int main(int argc, char* argv[])
 
         sf::RenderWindow window(sf::VideoMode(500, 500), "App", sf::Style::Default, settings);
 
-        Player player;
-        Obstacle obstacle(100, 30, 0, 200);
+        Collider collider;
+
+        auto obstacle = collider.createCircleBody(15, 50, 50);
 
         // Timing stuff
         sf::Clock clock;
@@ -104,17 +37,6 @@ int main(int argc, char* argv[])
                 }
 
                 while (lag >= MC_PER_TICK) {
-                        player.update();
-
-                        // Check for collision
-                        c2Manifold manifold;
-                        c2CircletoAABBManifold(player.body, obstacle.body,
-                                        &manifold);
-
-                        if (manifold.count > 0) {
-                                player.body.p.x -= manifold.normal.x * manifold.depths[0];
-                                player.body.p.y -= manifold.normal.y * manifold.depths[0];
-                        }
 
                         lag -= MC_PER_TICK;
                 }
@@ -124,8 +46,7 @@ int main(int argc, char* argv[])
                 window.setView(view);
 
                 window.clear(sf::Color(30, 30, 30));
-                window.draw(player);
-                window.draw(obstacle);
+                collider.draw(window);
                 window.display();
         }
 
